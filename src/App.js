cd58471fb4ddd414a62f9c9c18452adc0210c3fd
timurturbil <timurturbil@hotmail.com>
2021-03-21 21:48:10 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,51 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import profileImage from './img/myimage.jpg';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import React from 'react';
-import { useHistory } from "react-router-dom";
 
+import './App.css';
+
+import React from 'react';
+
+import NewPage from './anotherpage';
 function  App() {
-    const history = useHistory();
-    const navigateTo = () => history.push('./anotherpage.js');
+
     return (
       <div className="App">
-       <img src={profileImage} className="photo"/>
-       <h2 className="isim">Ben Timur Turbil Hoşgeldin</h2>
+      <NewPage/>
+       
       
-      <div className="same">
-      <Card className="heey">
-        <CardContent>
-  
-          <Typography variant="h5" component="h2">
-            Software Developer
-          </Typography>
-  
-          <Typography variant="body2" component="p">
-            {'Dart & Flutter & Javascript & React'},
-          </Typography>
-        </CardContent>
-      </Card>
-      </div>
-      <div className="same">
-      <Card className="heey">
-        <CardContent>
-  
-          <Typography variant="h5" component="h2">
-            my Javascript practices 
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small" onClick={navigateTo}>See the practices</Button>
-        </CardActions>
-      </Card>
-      </div>
-      </div>
+    </div>
+
     );
   
 }
--- src/anotherpage.js
+++ src/anotherpage.js
@@ -1,18 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 import profileImage from './img/myimage.jpg';
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
 import { useHistory } from "react-router-dom";
-
-function  yeniApp() {
-    const history = useHistory();
-    const navigateTo = () => history.push('/');
+import './App.css';
+function  YeniApp() {
     return (
       <div className="App">
        <img src={profileImage} className="photo"/>
@@ -21,20 +16,17 @@ function  yeniApp() {
       <div className="same">
       <Card className="heey">
         <CardContent>
-  
           <Typography variant="h5" component="h2">
             Software Developer
           </Typography>
-  
-          <Typography variant="body2" component="p">
-            {'Dart & Flutter & Javascript & React'},
+          <Typography variant="h10" component="h10">
+            Dart, Flutter, Javascript, React 
           </Typography>
         </CardContent>
       </Card>
       </div>
       </div>
     );
-  
 }
 
-export default yeniApp;
+export default YeniApp;
